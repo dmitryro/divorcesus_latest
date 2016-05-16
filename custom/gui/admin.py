f3326c0682fb6b993e3da0f1211fc3e9939f3d70
root <root@s16809674.onlinehome-server.com>
2016-05-16 00:03:27 -0500

    slideshow configurable now

--- custom/gui/admin.py
+++ custom/gui/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from models import Slide
+from forms import SlideForm
+from imagekit.admin import AdminThumbnail
 
 class SlideAdmin(admin.ModelAdmin):
+    form = SlideForm
     fieldsets = ((None, {'fields': ['title','text','link','slide']}),)
     list_display = ('id','title','text','link','slide_thumbnail')
     list_editable = ('title','text','link')
-
+    slide_thumbnail = AdminThumbnail(image_field='slide_thumbnail')
     class Meta:
          verbose_name = 'Slide'
          verbose_name_plural = 'Slides'
--- custom/gui/views.py
+++ custom/gui/views.py
@@ -38,6 +38,7 @@ from custom.utils.models import Logger
 from custom.users.models import MileStone
 from custom.users.models import Advantage
 from custom.users.models import AdvantageLink
+from custom.gui.models import Slide
 
 @ensure_csrf_cookie
 def environment(**options):
@@ -56,6 +57,7 @@ def index(request):
 def home(request):
     milestones = MileStone.objects.all()
     advantage_links = AdvantageLink.objects.filter(advantage_id=1)
+    slides = Slide.objects.all()
 
     if request.user.is_authenticated():
         logout=True
@@ -86,6 +88,7 @@ def home(request):
                                            'user_id':user_id,
                                            'first':first_name,
                                            'last':last_name,
+                                           'slides':slides,
                                            'milestones':milestones,
                                            'advantage_links':advantage_links,
                                            'profile_image':profile_image_path})
